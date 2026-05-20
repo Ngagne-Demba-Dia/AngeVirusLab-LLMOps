e40bed6f2aedf4c643b03e54a2c263aa3b39a5d1
Ngagne Demba Dia <xamteksam@gmail.com>
2026-05-20 10:37:10 +0000

    Lab16 — Rust AMSI bypass : patch AmsiScanBuffer E_INVALIDARG

---
+++ pilier-3-casm-embedded/lab16-rust-amsi-bypass/amsi_bypass/target/release/build/winapi-0a550087c2d93cb8/build_script_build-0a550087c2d93cb8.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/ngdia/Downloads/programme_angevirus/AngeVirusLab-LLMOps/pilier-3-casm-embedded/lab16-rust-amsi-bypass/amsi_bypass/target/release/build/winapi-0a550087c2d93cb8/build_script_build-0a550087c2d93cb8.d: /home/angevirus/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/winapi-0.3.9/build.rs
+
+/mnt/c/Users/ngdia/Downloads/programme_angevirus/AngeVirusLab-LLMOps/pilier-3-casm-embedded/lab16-rust-amsi-bypass/amsi_bypass/target/release/build/winapi-0a550087c2d93cb8/build_script_build-0a550087c2d93cb8: /home/angevirus/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/winapi-0.3.9/build.rs
+
+/home/angevirus/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/winapi-0.3.9/build.rs:
---
+++ pilier-3-casm-embedded/lab16-rust-amsi-bypass/amsi_bypass/target/release/build/winapi-x86_64-pc-windows-gnu-1cd67325fd1f269e/build_script_build-1cd67325fd1f269e.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/ngdia/Downloads/programme_angevirus/AngeVirusLab-LLMOps/pilier-3-casm-embedded/lab16-rust-amsi-bypass/amsi_bypass/target/release/build/winapi-x86_64-pc-windows-gnu-1cd67325fd1f269e/build_script_build-1cd67325fd1f269e.d: /home/angevirus/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/winapi-x86_64-pc-windows-gnu-0.4.0/build.rs
+
+/mnt/c/Users/ngdia/Downloads/programme_angevirus/AngeVirusLab-LLMOps/pilier-3-casm-embedded/lab16-rust-amsi-bypass/amsi_bypass/target/release/build/winapi-x86_64-pc-windows-gnu-1cd67325fd1f269e/build_script_build-1cd67325fd1f269e: /home/angevirus/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/winapi-x86_64-pc-windows-gnu-0.4.0/build.rs
+
+/home/angevirus/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/winapi-x86_64-pc-windows-gnu-0.4.0/build.rs:
